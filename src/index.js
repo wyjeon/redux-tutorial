6283531393b4cf897e6890bbfa12d0b28295814c
wyjeon <wyjeon.info@gmail.com>
2021-02-20 21:37:56 +0900

    액션 생성 함수 정의

--- src/index.js
+++ src/index.js
@@ -6,3 +6,8 @@ const switchButton = document.getElementById('switch-btn');
 const counterHeadings = document.getElementsByTagName('h1')[0];
 const plusButton = document.getElementById('plus-btn');
 const minusButton = document.getElementById('minus-btn');
+
+// 액션 생성 함수 정의
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
